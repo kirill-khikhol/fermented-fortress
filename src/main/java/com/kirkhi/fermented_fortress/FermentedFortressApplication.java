139963f3fb6fdaf9676ca2b66fbdeb43370d544d
kir_khi <kir_khi@me.com>
2026-04-06 12:28:30 +0300

    initial commit

---
+++ src/main/java/com/kirkhi/fermented_fortress/FermentedFortressApplication.java
@@ -0,0 +1,13 @@
+package com.kirkhi.fermented_fortress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FermentedFortressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FermentedFortressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirkhi/fermented_fortress/FermentedFortressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirkhi.fermented_fortress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FermentedFortressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
